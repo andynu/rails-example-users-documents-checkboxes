bdb14af786fa36ebec832e0bcb8b29c673618ea6
Andy <andynu@gmail.com>
2025-02-26 12:20:00 -0500

    add nested attributes between user/document

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.expect(user: [ :name ])
+      params.require(:user).permit(:name, document_ids: [])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :user_documents
   has_many :documents, through: :user_documents
+  accepts_nested_attributes_for :user_documents, allow_destroy: true
 end
